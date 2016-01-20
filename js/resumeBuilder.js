94dfd46cc305591a2a57f5ef886a17cf350def37
Lucy Moore <lucyviolet.moore@gmail.com>
2016-01-20 21:16:30 +1300

    education added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,12 @@
 
 
-// object containg personal information and contact details
+// the bio object contains  personal information and contact details
 var bio = {
     name: "Lucy Moore",
-    role: "web developer",
+    role: "Web/Software Developer",
     welcomeMsg: "Hello All! Welcome to my resume",
     skills: ["Java", "HTML5", "javaScript", "UML", "SQL", "CSS"],
-    image: "image/Lucy.jpg",
+    image: 'image/fry.jpg',
     contact: { 
       "phone": "0272741511",
       "email": "lucyviolet.moore@gmail.com",
@@ -14,18 +14,48 @@ var bio = {
       "location": "Wellington, New Zealand",
       "github": "LucyMoore",
       
-            },
-    
-    function displayBio() {
-        //name and role displayed on resume
-        var formattedName = HTMLheaderName.replace("%data%","Lucy Moore")
-        $("#header").prepend(formattedName)
-        var formattedRole = HTMLheaderRole.replace("%data%","Web/Software Developer")
-        $("#header").append(formattedRole)
+    },
+};
 
-              };
-  };
-displayBio();
+bio.displayBio = function() {
+
+    var formattedRole = HTMLheaderRole.replace("%data%",bio.role)
+    $("#header").prepend(formattedRole)
+
+    formattedName = HTMLheaderName.replace("%data%",bio.name)
+    $("#header").prepend(formattedName)
+
+  // contact details at the top of the page:
+    var formattedPhone = HTMLmobile.replace("%data%",bio.contact.phone)  
+    $("#topContacts").append(formattedPhone)
+    var formattedEmail = HTMLemail.replace("%data%",bio.contact.email)  
+    $("#topContacts").append(formattedEmail)
+    var formattedGit = HTMLgithub.replace("%data%",bio.contact.github)  
+    $("#topContacts").append(formattedGit)
+    var formattedLocation = HTMLlocation.replace("%data%",bio.contact.location)  
+    $("#topContacts").append(formattedLocation)
+    var formattedImage = HTMLbioPic.replace("%data%",bio.image)
+    $("#topContacts").append(formattedImage)
+
+  // contact details at the bottom of the page:
+    var formattedPhone = HTMLmobile.replace("%data%",bio.contact.phone)  
+    $("#footerContacts").append(formattedPhone)
+    var formattedEmail = HTMLemail.replace("%data%",bio.contact.email)  
+    $("#footerContacts").append(formattedEmail)
+    var formattedGit = HTMLgithub.replace("%data%",bio.contact.github)  
+    $("#footerContacts").append(formattedGit)
+    var formattedLocation = HTMLlocation.replace("%data%",bio.contact.location)  
+    $("#footerContacts").append(formattedLocation)
+
+  // displaying the skills:
+    $("#header").append(HTMLskillsStart);
+      for(index = 0; index < bio.skills.length; index++){
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[index]);
+        $("#header").append(formattedSkills);
+      }
+};
+//call the function
+bio.displayBio();
 
 // object containing work history information
 var work = {
@@ -54,6 +84,28 @@ var work = {
    ]
   };
 
+//function to display work history to the resume
+work.displayWork = function(){
+
+    for(var key in work.job){
+
+      $("#workExperience").append(HTMLworkStart);
+      
+      var formattedEmp = HTMLworkEmployer.replace("%data%", work.job[key].employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%", work.job[key].title);
+      var formattedEmpTitle = formattedEmp + formattedTitle
+      $(".work-entry:last").append(formattedEmpTitle)
+      var formattedLocation = HTMLworkLocation.replace("%data%", work.job[key].location);
+      $(".work-entry:last").append(formattedLocation)
+      var formattedDates = HTMLworkDates.replace("%data%", work.job[key].dates);
+      $(".work-entry:last").append(formattedDates)
+      var formattedDescription = HTMLworkDescription.replace("%data%", work.job[key].description);
+      $(".work-entry:last").append(formattedDescription)
+    }
+};
+// calling the function
+work.displayWork();
+
 // object holding all study information
 var education = {
     schools: [
@@ -64,16 +116,16 @@ var education = {
            qualification: "Diploma in Software Development (Java)"
   },
   {
-           name: "Natcol Design Technology",
-           years: "2007 - 2008",
-           schoolCity: "Wellington",
-     qualification: "Diploma in Video Post Production",
+          name: "Natcol Design Technology",
+          years: "2008",
+          schoolCity: "Wellington",
+          qualification: "Diploma in Video Post Production",
   },
   {
-           name: "Natcol Design Technology",
-           years: "2007 - 2008",
-           schoolCity: "Wellington",
-     qualification: "Diploma in Multi Media",
+          name: "Natcol Design Technology",
+          years: "2007",
+          schoolCity: "Wellington",
+          qualification: "Diploma in Multi Media",
   }
     ],
   
@@ -86,36 +138,27 @@ var education = {
   ]
   };
 
- function to display work history to the pageon the resume
-function displayWork(){
-  for(var key in work.job){
+ education.displayEdu = function(){
+    for(var key in education.schools){
 
-    $("#workExperience").append(HTMLworkStart);
+          $("#education").append(HTMLschoolStart);
+          
+          var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[key].name);
+          $(".education-entry:last").append(formattedSchoolName)
+
+          var formattedYears = HTMLschoolDates.replace("%data%", education.schools[key].years);
+          $(".education-entry:last").append(formattedYears)
+
+          var formattedSchoolLocal = HTMLschoolLocation.replace("%data%", education.schools[key].schoolCity)
+          $(".education-entry:last").append(formattedSchoolLocal)
+
+          var formattedQualification = HTMLschoolMajor.replace("%data%", education.schools[key].qualification);
+          $(".education-entry:last").append(formattedQualification)
     
-    var formattedEmp = HTMLworkEmployer.replace("%data%", work.job[key].employer);
-
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.job[key].title);
-    
-    var formattedEmpTitle = formattedEmp + formattedTitle
-
-    $(".work-entry:last").append(formattedEmpTitle)
-
-    var formattedLocation = HTMLworkLocation.replace("%data%", work.job[key].location);
-
-    $(".work-entry:last").append(formattedLocation)
-
-   var formattedDates = HTMLworkDates.replace("%data%", work.job[key].dates);
-
-   $(".work-entry:last").append(formattedDates)
-
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.job[key].description);
-
-    $(".work-entry:last").append(formattedDescription)
-}
+    }
 };
-// calling the function
-displayWork();
 
+education.displayEdu(); 
 
 //display the internationalize button
 $("#main").append(internationalizeButton)
